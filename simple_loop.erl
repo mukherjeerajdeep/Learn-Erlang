85aeb3286dd9af6316d08930635956a0a47f6502
Rajdeep Mukherjee <rajdeep.mukherjee@ericsson.com>
2023-10-15 12:52:38 +0200

    Exercise1

--- simple_loop.erl
+++ simple_loop.erl
@@ -1,26 +1,26 @@
 -module(simple_loop).
 
-% -export([hello_world/0]).
-% -export([create_list/0]).
-% -export([complex_list_rec/0]).
-% -export([complex_list_rec_val/0]).
+-export([hello_world/0]).
+-export([create_list/0]).
+-export([complex_list_rec/0]).
+-export([complex_list_rec_val/0]).
 -export([get_list_size/1]).
 -export([get_record/1, print_list_of_map/1]).
 
-% hello_world() ->
-%     io:format("Hello world!").
+hello_world() ->
+    io:format("Hello world!").
 
-% create_list() ->
-%     List = [X || X <- [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], X rem 2 =:= 0],
-%     List.
+create_list() ->
+    List = [X || X <- [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], X rem 2 =:= 0],
+    List.
 
-% complex_list_rec() ->
-%     List = [begin #{paramId => 0, paramValue => 0} end || _ <- lists:seq(1, 29)],
-%     List.
+complex_list_rec() ->
+    List = [begin #{paramId => 0, paramValue => 0} end || _ <- lists:seq(1, 29)],
+    List.
 
-% complex_list_rec_val() ->
-%     List = [#{paramId => N, paramValue => N} || N <- lists:seq(1, 29)],
-%     List.
+complex_list_rec_val() ->
+    List = [#{paramId => N, paramValue => N} || N <- lists:seq(1, 29)],
+    List.
 
 get_list_size(List) ->
     get_list_size(List, 0).
